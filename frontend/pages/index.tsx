b769f59509655401ebf90043bcd411a01d7e2316
maxrchung <maxrchung@gmail.com>
2021-05-09 16:08:36 -0700

    Add web socket client

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export default function Home(): JSX.Element {
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = new WebSocket("ws://localhost:3010");
+    socket.current.onmessage = message => {
+      console.log(message.data);
+    };
+  }, []);
+
   return (
     <div className="container">
       <Head>
         <title>Retro</title>
         <meta name="description" content="A retro tool made with some cool stuff" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <button className="border-2 border-black p-2">
