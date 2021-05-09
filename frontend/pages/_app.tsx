c333c1a997a59774f5d59aac4fe7e3e0fffb48ea
maxrchung <maxrchung@gmail.com>
2021-05-09 11:34:37 -0700

    Put in basic button

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,6 @@ import { AppProps } from 'next/app';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 
-export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
 }
