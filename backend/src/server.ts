4c4fe419a27d20ba591f7ced0418a62ba3780bd4
maxrchung <maxrchung@gmail.com>
2021-05-09 18:45:27 -0700

    Return some simple stuff back from ws server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,13 @@ import WebSocket from 'ws';
 
 const server = new WebSocket.Server({ port: 3010 });
 
+const columns = [
+  "Column1",
+  "Column2",
+  "Column3"
+]
+
 server.on('connection', ws => {
   ws.on('message', message => console.log('received: %s', message));
-  ws.send('something');
+  ws.send(JSON.stringify(columns));
 });
