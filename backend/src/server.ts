1bdae4bc6e6c5c784511590e1ed38ce14d72918a
maxrchung <maxrchung@gmail.com>
2021-05-16 12:32:40 -0700

    Add some test comment data

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,10 +5,13 @@ import { v4 as uuidv4 } from 'uuid'
 const server = new WebSocket.Server({ port: 3010 })
 
 const columns = [
-  { uuid: uuidv4(), name: "Column1" },
-  { uuid: uuidv4(), name: "Column2" },
-  { uuid: uuidv4(), name: "Column3" },
-  { uuid: uuidv4(), name: "Column4" }
+  { uuid: uuidv4(), name: "Column1", comments: [
+    { uuid: uuidv4(), value: "Hey what's up my dude" },
+    { uuid: uuidv4(), value: "Lmao yo it was pretty chill!" },
+  ] },
+  { uuid: uuidv4(), name: "Column2", comments: [] },
+  { uuid: uuidv4(), name: "Column3", comments: [] },
+  { uuid: uuidv4(), name: "Column4", comments: [] }
 ]
 
 server.on('connection', ws => {
