b188b290767e3c3533c2bb14049b25845e9cf931
maxrchung <maxrchung@gmail.com>
2021-05-28 03:19:25 -0700

    Format message sent from server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,26 @@
 import WebSocket from 'ws'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 } from 'uuid'
 
 
 const server = new WebSocket.Server({ port: 3010 })
 
 const columns = [
-  { uuid: uuidv4(), name: "Column1", comments: [
-    { uuid: uuidv4(), value: "Hey what's up my dude" },
-    { uuid: uuidv4(), value: "Lmao yo it was pretty chill!" },
+  { uuid: v4(), name: "Column1", comments: [
+    { uuid: v4(), value: "Hey what's up my dude" },
+    { uuid: v4(), value: "Lmao yo it was pretty chill!" },
   ] },
-  { uuid: uuidv4(), name: "Column2", comments: [] },
-  { uuid: uuidv4(), name: "Column3", comments: [] },
-  { uuid: uuidv4(), name: "Column4", comments: [] }
+  { uuid: v4(), name: "Column2", comments: [] },
+  { uuid: v4(), name: "Column3", comments: [] },
+  { uuid: v4(), name: "Column4", comments: [] }
 ]
+
+const formatMessage = (type: string, payload) =>
+  JSON.stringify({
+    type: `retro/${type}`,
+    payload
+  })
 
 server.on('connection', ws => {
   ws.on('message', message => console.log('received: %s', message))
-  ws.send(JSON.stringify(columns))
+  ws.send(formatMessage('getAllColumns', columns))
 })
