bb47aa86addf0a186c870f64a2a82d90bab05e9d
maxrchung <maxrchung@gmail.com>
2021-06-13 22:53:42 -0700

    Refactor column add to use Card

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import React, { useContext, useState } from 'react'
 import { useAppSelector } from 'app/hooks'
 import { SocketContext } from 'app/socketContext'
+import Card from 'components/card'
+import IconButton from 'components/iconButton'
+import { PlusIcon } from '@heroicons/react/outline'
 
 export default function Home(): JSX.Element {
   const [columnName, setColumnName] = useState('')
@@ -35,17 +38,20 @@ export default function Home(): JSX.Element {
         )}
 
         <div>
-          <input
-            className="block border-2 border-black"
-            onChange={e => setColumnName(e.target.value)}
-            value={columnName}
+          <Card
+            content={
+              <input
+                className="block border-2 border-black"
+                onChange={e => setColumnName(e.target.value)}
+                value={columnName}
+              />
+            }
+            buttons={
+              <IconButton onClick={() => handleAddColumn()}>
+                <PlusIcon />
+              </IconButton>
+            }
           />
-          <button
-            className="border-2 border-black p-2"
-            onClick={() => handleAddColumn()}
-          >
-            + Column
-          </button>
         </div>
       </div>
     </div>
