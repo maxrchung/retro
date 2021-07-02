a2ccf57db112e8fd5a49ba0f11e4021b429e0970
maxrchung <maxrchung@gmail.com>
2021-07-01 20:26:17 -0700

    Use cyan for input colors

--- frontend/components/iconButton.tsx
+++ frontend/components/iconButton.tsx
@@ -10,7 +10,7 @@ export default function IconButton(props: IconButtonProps): JSX.Element {
     <div className="flex items-center">
       <button
         // Ok https://github.com/tailwindlabs/tailwindcss/issues/1231
-        className="h-5 w-5 bg-white rounded border-2 focus:outline-none border-pink-500 text-pink-500 focus:border-pink-300 focus:text-pink-300 hover:border-pink-300 hover:text-pink-300"
+        className="h-5 w-5 bg-white rounded border-2 focus:outline-none border-blue-500 text-blue-500 focus:border-blue-300 focus:text-blue-300 hover:border-blue-300 hover:text-blue-300"
         onClick={props.onClick}
       >
         {props.children}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: colors.cyan
+      }
+    },
+  },
   variants: {
     extend: {},
   },
