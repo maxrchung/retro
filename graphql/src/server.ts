e7d2102f81ed470e502a2c278cabd6d1a2e4854d
maxrchung <maxrchung@gmail.com>
2021-07-07 22:11:19 -0700

    Following along and playing with some graphql tutorials

---
+++ graphql/src/server.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from 'apollo-server'
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => null
+  }
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  )
