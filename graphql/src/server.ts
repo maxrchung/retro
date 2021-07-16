fb8b746bffaa38af60cfc0fb2197c8a867e166f9
maxrchung <maxrchung@gmail.com>
2021-07-15 17:37:48 -0700

    Add prisma

--- graphql/src/server.ts
+++ graphql/src/server.ts
@@ -1,21 +1,78 @@
 import { ApolloServer } from 'apollo-server'
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+import fs from 'fs'
+import path from 'path'
 
-// 2
+interface Link {
+  id: string,
+  url: string,
+  description: string
+}
+
+interface PostArgs {
+  description: string,
+  url: string
+}
+
+interface UpdateLinkArgs {
+  id: string,
+  description: string,
+  url: string
+}
+
+interface DeleteLinkArgs {
+  id: string
+}
+
+let links: Link[] = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => null
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links,
+    link: (id: string) => links.find(link => link.id == id)
+  },
+  Mutation: {
+    // 2
+    post: (parent: null, args: PostArgs) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent: null, args: UpdateLinkArgs) => {
+      const link = links.find(link => link.id == args.id)
+      if (typeof link === 'undefined') {
+        return undefined
+      }
+      if (args.url) {
+        link.url = args.url
+      }
+      if (args.description) {
+        link.description = args.description
+      }
+      return link
+    },
+    deleteLink: (parent: null, args: DeleteLinkArgs) => {
+      const link = links.find(link => link.id == args.id)
+      if (typeof link === 'undefined') {
+        return undefined
+      }
+      links = links.filter(link => link.id != args.id)
+      return link
+    }
   }
 }
 
-// 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
 })
 
