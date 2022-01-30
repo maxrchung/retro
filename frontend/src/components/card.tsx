12c3bda97c7ef5c1360ab8815cfd65f7551e9b60
maxrchung <maxrchung@gmail.com>
2022-01-30 05:14:54 -0800

    add initial drop code

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import React from 'react'
-import { useDrag } from 'react-dnd'
+import { useDrag, useDrop } from 'react-dnd'
 import { ItemTypes } from './ItemTypes'
 
 interface CardProps {
@@ -10,16 +10,33 @@ interface CardProps {
 
 export default function Card(props: CardProps): JSX.Element {
   const { content, buttons } = props
-  const [{ isDragging }, dragReference] = useDrag(() => ({
+  const [{ isDragging }, dragRef] = useDrag(() => ({
     type: ItemTypes.Card,
     collect: (monitor) => ({
       isDragging: monitor.isDragging()
     })
   }))
 
+  const [{ canDrop, isOver }, dropRef] = useDrop(() => ({
+    accept: ItemTypes.Card,
+    hover: (item, monitor) => {
+      console.log('hovered on card')
+    },
+    drop: (item, monitor) => {
+      console.log('dropped on card')
+    },
+    collect: (monitor) => ({
+      canDrop: monitor.canDrop(),
+      isOver: monitor.isOver()
+    })
+  }))
+
   return (
     <div
-      ref={dragReference}
+      ref={(ref) => {
+        dragRef(ref)
+        dropRef(ref)
+      }}
       className={classNames(
         'p-2 min-w-0 flex justify-between items-center cursor-grab',
         {
