c11cafd9d578e029f382d324e7643567f0991661
maxrchung <maxrchung@gmail.com>
2022-05-07 22:35:35 -0700

    add cdk for ddb

---
+++ cdk/lib/retro-stack.ts
@@ -0,0 +1,23 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+
+export class RetroStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    const table = new dynamodb.Table(this, 'retro-table', {
+      tableName: 'retro-table',
+      pointInTimeRecovery: true,
+      partitionKey: { name: 'retroId', type: dynamodb.AttributeType.STRING }
+    })
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
