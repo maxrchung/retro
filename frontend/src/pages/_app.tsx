5f26d8eadcb9bc6cc995f21d78bf01911cfa6748
maxrchung <maxrchung@gmail.com>
2022-05-22 16:16:40 -0700

    fix home page flex

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -75,7 +75,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
             </a>
           </div>
         </nav>
-        <main className="p-3 flex overflow-hidden flex-auto">
+        <main className="p-3 flex overflow-hidden">
           <ApolloProvider client={client}>
             <Component {...pageProps} />
           </ApolloProvider>
