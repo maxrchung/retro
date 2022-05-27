2b5014d9373cbd6f4fa29fb0e1d58181973fbf98
maxrchung <maxrchung@gmail.com>
2022-05-27 01:22:23 -0700

    separate content from retro

--- frontend/src/components/Retro.tsx
+++ frontend/src/components/Retro.tsx
@@ -1,12 +1,7 @@
-import Column from 'components/Column'
 import Head from 'next/head'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../state/hooks'
-import IconButton from 'components/IconButton'
-import { PlusSmIcon } from '@heroicons/react/outline'
-import ColumnHeader from 'components/ColumnHeader'
 import {
-  useCreateColumn,
   useGetRetro,
   useColumnsUpdated,
   useNameUpdated,
@@ -14,11 +9,7 @@ import {
 } from 'graphql/client'
 import { useRouter } from 'next/router'
 import { actions } from 'state/retroSlice'
-import Timer from './Timer'
-import InputContainer from './InputContainer'
-import RetroHeader from './RetroHeader'
-import TextArea from './TextArea'
-import { isKeyEnterOnly } from 'utils'
+import RetroContent from './RetroContent'
 
 export default function Retro(): JSX.Element {
   const router = useRouter()
@@ -32,12 +23,7 @@ export default function Retro(): JSX.Element {
   const { data: dataName } = useNameUpdated({ retroId }, !retroId)
   const { data: dataTimer } = useTimerUpdated({ retroId }, !retroId)
 
-  const [columnName, setColumnName] = useState('')
-  const { name, columns } = useAppSelector((state) => state.retro)
-  const [createColumn] = useCreateColumn({
-    retroId,
-    columnName
-  })
+  const { name } = useAppSelector((state) => state.retro)
 
   useEffect(() => {
     if (dataGet) {
@@ -63,13 +49,6 @@ export default function Retro(): JSX.Element {
     }
   }, [dataTimer])
 
-  const submitCreateColumn = () => {
-    if (columnName.length > 0) {
-      createColumn()
-      setColumnName('')
-    }
-  }
-
   if (error) {
     return <>{`Failed to load retro: ${error.message}`}</>
   }
@@ -85,55 +64,7 @@ export default function Retro(): JSX.Element {
         <meta name="description" content="A simple retrospective tool." />
       </Head>
 
-      <div className="flex flex-col overflow-hidden">
-        <div className="flex justify-between gap-3 p-3">
-          <RetroHeader />
-          <Timer />
-        </div>
-
-        {/* Wacky padding margin hacks to handle overflow-auto button clipping and left-most column indicator */}
-        <div className="flex overflow-x-auto overflow-y-hidden px-1 pb-3">
-          <div className="flex">
-            {columns.map((column, index) => (
-              <Column key={column.id} column={column} index={index} />
-            ))}
-
-            {/* flex is needed for proper column width */}
-            <div className="flex">
-              <div className="flex flex-col w-80 mx-1">
-                <div className="bg-gray-100 rounded mx-1 p-3">
-                  <ColumnHeader>
-                    <InputContainer
-                      content={
-                        <div className="flex">
-                          <TextArea
-                            onKeyDown={(e) => {
-                              if (isKeyEnterOnly(e)) {
-                                submitCreateColumn()
-                                e.preventDefault()
-                              }
-                            }}
-                            onChange={(e) => setColumnName(e.target.value)}
-                            value={columnName}
-                            placeholder="Column"
-                          />
-                        </div>
-                      }
-                      button={
-                        <IconButton
-                          icon={<PlusSmIcon />}
-                          onClick={() => submitCreateColumn()}
-                          title="Create column"
-                        />
-                      }
-                    />
-                  </ColumnHeader>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <RetroContent />
     </div>
   )
 }
