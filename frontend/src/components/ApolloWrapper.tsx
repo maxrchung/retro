1c9ab0727d38fe5af8aa6c143afb87be66b15d0e
maxrchung <maxrchung@gmail.com>
2022-06-06 02:24:55 -0700

    usememo on client creation

--- frontend/src/components/ApolloWrapper.tsx
+++ frontend/src/components/ApolloWrapper.tsx
@@ -12,7 +12,7 @@ import { getMainDefinition } from '@apollo/client/utilities'
 import { GRAPHQL_WEB_SOCKET_URI } from '../constants'
 import { GRAPHQL_HTTP_URI } from '../constants'
 import { AppProps } from 'next/app'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useAppDispatch } from 'state/hooks'
 import { actions } from 'state/retroSlice'
 import ErrorBar from './ErrorBar'
@@ -39,54 +39,56 @@ export default function ApolloWrapper({
 }: AppProps): JSX.Element {
   const dispatch = useAppDispatch()
 
-  const errorLink = onError(({ response, graphQLErrors, networkError }) => {
-    const errors = []
-    if (graphQLErrors) {
-      console.dir(graphQLErrors)
-      graphQLErrors.forEach(({ message, extensions }) =>
-        errors.push(
-          extensions?.code === 'INTERNAL_SERVER_ERROR'
-            ? 'Internal server error'
-            : message
-        )
-      )
-    }
-    if (networkError) {
-      errors.push(
-        networkError.message.length > 0
-          ? networkError.message
-          : 'Internal server error'
-      )
-    }
-    dispatch(actions.setErrors(errors))
-
-    // Ignore errors so that unhandled exception isn't thrown at the component level
-    // https://www.apollographql.com/docs/react/data/error-handling/#ignoring-errors
-    if (response) {
-      response.errors = undefined
-    }
-  })
-
-  // https://www.apollographql.com/docs/react/data/subscriptions/#3-split-communication-by-operation-recommended
-  const splitLink = process.browser
-    ? split(
-        ({ query }) => {
-          const definition = getMainDefinition(query)
-          dispatch(actions.clearErrors())
-          return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
+  const client = useMemo(() => {
+    const errorLink = onError(({ response, graphQLErrors, networkError }) => {
+      const errors = []
+      if (graphQLErrors) {
+        console.dir(graphQLErrors)
+        graphQLErrors.forEach(({ message, extensions }) =>
+          errors.push(
+            extensions?.code === 'INTERNAL_SERVER_ERROR'
+              ? 'Internal server error'
+              : message
           )
-        },
-        webSocketLink as WebSocketLink,
-        httpLink
-      )
-    : httpLink
+        )
+      }
+      if (networkError) {
+        errors.push(
+          networkError.message.length > 0
+            ? networkError.message
+            : 'Internal server error'
+        )
+      }
+      dispatch(actions.setErrors(errors))
 
-  const client = new ApolloClient({
-    link: from([errorLink, splitLink]),
-    cache: new InMemoryCache()
-  })
+      // Ignore errors so that unhandled exception isn't thrown at the component level
+      // https://www.apollographql.com/docs/react/data/error-handling/#ignoring-errors
+      if (response) {
+        response.errors = undefined
+      }
+    })
+
+    // https://www.apollographql.com/docs/react/data/subscriptions/#3-split-communication-by-operation-recommended
+    const splitLink = process.browser
+      ? split(
+          ({ query }) => {
+            const definition = getMainDefinition(query)
+            dispatch(actions.clearErrors())
+            return (
+              definition.kind === 'OperationDefinition' &&
+              definition.operation === 'subscription'
+            )
+          },
+          webSocketLink as WebSocketLink,
+          httpLink
+        )
+      : httpLink
+
+    return new ApolloClient({
+      link: from([errorLink, splitLink]),
+      cache: new InMemoryCache()
+    })
+  }, [])
 
   return (
     <ApolloProvider client={client}>
