2b815362b63b482c42bf0f879cb61098540e9412
maxrchung <maxrchung@gmail.com>
2022-06-07 19:49:01 -0700

    set up graphql/constants

--- frontend/src/pages/[id].tsx
+++ frontend/src/pages/[id].tsx
@@ -11,6 +11,7 @@ import { useRouter } from 'next/router'
 import { actions } from 'state/retroSlice'
 import Retro from 'components/Retro'
 import { getCookie } from 'cookies-next'
+import { RETRO_CONNECTION_ID } from 'graphql/constants'
 
 export default function Id(): JSX.Element {
   const router = useRouter()
@@ -24,7 +25,7 @@ export default function Id(): JSX.Element {
   const { data: dataOptions } = useOptionsUpdated({ retroId }, !retroId)
   const { data: dataRemoved } = useRetroRemoved({ retroId }, !retroId)
   const { name } = useAppSelector((state) => state.retro)
-  const connectionId = getCookie('retro-connection-id') ?? ''
+  const connectionId = getCookie(RETRO_CONNECTION_ID) ?? ''
 
   useEffect(() => {
     dispatch(actions.setConnectionId(connectionId.toString()))
