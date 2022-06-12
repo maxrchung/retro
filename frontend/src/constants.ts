c82a6ffb1c6a08a47b2293dacf47013a059082cf
maxrchung <maxrchung@gmail.com>
2022-06-11 20:31:54 -0700

    set up basic docker file

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 // Need to preface environment variables with NEXT_PUBLIC: https://nextjs.org/docs/basic-features/environment-variables
-export const GRAPHQL_HTTP_URI = process.env.NEXT_PUBLIC_GRAPHQL_HTTP_URI ?? ''
+export const GRAPHQL_HTTP_URI = process.env.NEXT_PUBLIC_GRAPHQL_HTTP_URI || ''
 export const GRAPHQL_WEB_SOCKET_URI =
-  process.env.NEXT_PUBLIC_GRAPHQL_WEB_SOCKET_URI ?? ''
+  process.env.NEXT_PUBLIC_GRAPHQL_WEB_SOCKET_URI || ''
